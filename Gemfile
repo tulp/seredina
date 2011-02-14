60f738651542a25ad1bfacb064849d84bde43440
Maxim Kaschenko <kaschenkomaksim@gmail.com>
2011-02-14 16:12:21 +0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+gem 'rails',  '3.0.4'
+gem 'mysql2', '0.2.6'
+gem 'haml',   '3.0.25'
+
+group :development do
+  gem 'mongrel',       '1.1.5'
+  gem 'wirble',        '0.1.3'
+  gem 'awesome_print', '0.2.1', :require => 'ap'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gold::Application.config.secret_token = '4779dc41a6b0210e3bef71007141def48f0794f9ca0baa612aabf3ee3b5b0649c02a8250acb9961bbea6968495ce17e14c54d0bcedf1805f65021bb909c39b3f'
