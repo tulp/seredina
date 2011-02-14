5b2eb1c19918ecf6e967a8869e2493a8e373cfdf
Maxim Kaschenko <kaschenkomaksim@gmail.com>
2011-02-15 00:01:23 +0600

    Map controls

--- public/javascripts/app/home/index.js
+++ public/javascripts/app/home/index.js
@@ -1,5 +1,14 @@
 YMaps.jQuery(function () {
-  var map = new YMaps.Map(document.getElementById('yandex_maps'));
+  var map         = new YMaps.Map(document.getElementById('yandex_maps'));
+  var zoomControl = new YMaps.Zoom({ customTips: [{ index: 9,  value: 'Город' },
+                                                  { index: 13, value: 'Улица' },
+                                                  { index: 17, value: 'Дом' }] });
+  var typeControl   = new YMaps.TypeControl();
+  var searchControl = new YMaps.SearchControl();
+
+  map.addControl(zoomControl);
+  map.addControl(typeControl);
+  map.addControl(searchControl);
 
   map.setCenter(new YMaps.GeoPoint(37.64, 55.76), 10);
 })
