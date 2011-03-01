bb50cbecd7e389797cc081ab39f2467b3fc0b87d
Maxim Kaschenko <kaschenkomaksim@gmail.com>
2011-03-01 14:07:49 +0600

    Raw reviews for market information

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,9 @@ class HomeController < ApplicationController
     respond_to do |format|
       format.html
       format.json do
-        render :json => Category.with_markets.to_json(:except => :id,
-                                                      :include => { :markets => { :except => [:id, :category_id, :address, :description, :subject] } })
+        render :json => Category.with_markets_and_reviews.to_json(:except => :id,
+                                                                  :include => { :markets => { :except => [:id, :category_id, :address, :description, :subject],
+                                                                                              :include => { :reviews => { :only => [:text, :rating] } } } })
       end
     end
   end
