f6b602f11eee95cb9f2b5f0f26b927170b845e96
Aibeck <aibeck@aibeck-laptop.(none)>
2011-03-03 15:11:31 +0600

    Created sidebar block

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     respond_to do |format|
-      format.html
+      format.html { render 'golden_middle/index.haml' }
       format.json do
         render :json => Category.with_markets_and_reviews.to_json(:except => :id,
                                                                   :include => { :markets => { :except => [:id, :category_id, :address, :description, :subject],
