115001a8f189e7a220fb5ed336b0302aaa8519c9
Oleg V Artamonov <oleg@artamonov.ru>
2011-03-15 15:41:25 +0600

    update mailer again

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MAILER_CONF = YAML.load_file(File.join("#{Rails.root}", 'config', 'mailer_config.yml'))
+#MAILER_CONF = YAML.load_file(File.join("#{Rails.root}", 'config', 'mailer_config.yml'))
 
 #ActionMailer::Base.delivery_method = :activerecord #For ar_send_mail
 ActionMailer::Base.delivery_method = :sendmail
