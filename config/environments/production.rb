2ef0368c6ef577b1dc2cfdd8ef7db7d8aa73125b
Oleg V Artamonov <oleg@artamonov.ru>
2011-03-15 15:40:24 +0600

    update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,6 @@ Gold::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'seredina.sitedock.ru' }
-  ActionMailer::Base.delivery_method = :sendmail
 
   # Enable threaded mode
   # config.threadsafe!
