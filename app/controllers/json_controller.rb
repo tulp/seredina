86b9b895f9e80f504ba2e129425a3ebc80914219
Ivan Nemytchenko <ivan@nemytchenko.ru>
2011-03-22 13:57:33 +0600

    old behaviour

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -3,11 +3,11 @@ class JsonController < ApplicationController
     if params[:id]
       markets = Market.fields_for_json.find_by_id(params[:id])
     else
-      if params[:category] == 'all'
+      # if params[:category] == 'all'
         markets = Market.fields_for_json
-      else
-        markets = Category.find_by_icon_image(params[:category]).markets.fields_for_json
-      end
+      # else
+        # markets = Category.find_by_icon_image(params[:category]).markets.fields_for_json
+      # end
     end
     render :json => markets.to_json(json_market_options)
   end
